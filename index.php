4ac77b81712c7d7785714f61d7ea9c391c30e5df
juanpabloruiz <pabloruiz1980@gmail.com>
2022-09-22 16:54:26 -0300

    aa

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ include('administracion/conexion.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
     <?php if (isset($_SESSION['correo'])) { ?>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
