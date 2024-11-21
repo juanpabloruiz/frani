1f607c4bade44ed7dbb8bfa3188878284001fb40
juanpabloruiz <pabloruiz1980@gmail.com>
2024-11-21 12:44:20 -0300

    Cambios

--- ventas.php
+++ ventas.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos | FRANI</title>
+    <title>Ventas | FRANI</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
