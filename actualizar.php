0f129779f26cfd29aa3d925566ef6bf6d7d2fec9
juanpabloruiz <pabloruiz1980@gmail.com>
2024-11-22 12:02:29 -0300

    Stock arreglado al actualizar

--- actualizar.php
+++ actualizar.php
@@ -2,9 +2,10 @@
 include('conexion.php');
 $id = $_POST['id'];
 $producto = $_POST['producto'];
+$stock = $_POST['stock'];
 $categoria = $_POST['categoria'];
 $precio = $_POST['precio'];
 $costo = $_POST['costo'];
-mysqli_query($conexion, "UPDATE productos SET producto = '$producto', categoria = '$categoria', precio = '$precio', costo = '$costo', fecha = NOW() WHERE id = '$id'");
+mysqli_query($conexion, "UPDATE productos SET producto = '$producto', stock = '$stock', categoria = '$categoria', precio = '$precio', costo = '$costo', fecha = NOW() WHERE id = '$id'");
 header('location:./');
 ?>
