a502d44fe42248f9ca272d4e0cc2d10605ea2f30
juanpabloruiz <pabloruiz1980@gmail.com>
2024-11-22 12:00:25 -0300

    Stock arreglado al insertar

--- insertar.php
+++ insertar.php
@@ -1,9 +1,10 @@
 <?php
 include('conexion.php');
 $producto = $_POST['producto'];
+$stock = $_POST['stock'];
 $categoria = $_POST['categoria'];
 $precio = $_POST['precio'];
 $costo = $_POST['costo'];
-mysqli_query($conexion, "INSERT INTO productos (producto, categoria, precio, costo, fecha) VALUES ('$producto', '$categoria', '$precio', '$costo', NOW())");
+mysqli_query($conexion, "INSERT INTO productos (producto, stock, categoria, precio, costo, fecha) VALUES ('$producto', '$stock', '$categoria', '$precio', '$costo', NOW())");
 header('location:./');
 ?>
