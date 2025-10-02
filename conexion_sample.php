26c3a17b6edc099b8a77a14bbfd28f1bac3e09f0
juanpabloruiz <pabloruiz1980@gmail.com>
2025-10-02 16:36:58 -0300

    Agrego conexion predeterminada

--- conexion_sample.php
+++ conexion_sample.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect('localhost', 'pablo', 'Soledad2025.', 'frani');
+$conexion = mysqli_connect('localhost', 'usuario', 'clave', 'basededatos');
 mysqli_set_charset($conexion, 'utf8');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
