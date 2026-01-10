3f870ea242208412f135736c71c4d0bba066a49d
juanpabloruiz <pabloruiz1980@gmail.com>
2026-01-10 00:57:01 -0300

    Cambios en UI de menú y galería

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/../conexion.php'; ?>
+<?php require_once __DIR__ . '/conexion.php'; ?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -17,10 +17,17 @@
 
 <body>
 
+    <!-- Cabecera -->
+    <header class="bg-primary p-4 text-white">
+        <a href="./">
+            <h1>Frani</h1>
+        </a>
+    </header>
+
     <!-- Menú -->
-    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Frani</a>
+    <nav class="navbar sticky-top navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container justify-content-center">
+            <a class="navbar-brand" href="<?= BASE_URL ?>">Frani</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -28,20 +35,13 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?= BASE_URL ?>">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>panel">Panel</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-
-    <!-- Cabecera -->
-    <header class="bg-primary p-4 text-white">
-        <a href="./">
-            <h1>Frani</h1>
-        </a>
-    </header>
 
     <div class="container my-4">
 
@@ -57,11 +57,10 @@
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h3 class="card-title">$ 20.000 Set de Arte Carpin-bara</h3>
+                            <p class="card-text">Son grandes , tienen 208 piezas. Para niños mayores a 3 años.</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
@@ -75,9 +74,10 @@
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a short card.</p>
+                            <h3 class="card-title">$ 2.000 Gorros navideños</h3>
+                            <p class="card-text">Salieron más gorritos navideños 🎅</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
@@ -91,11 +91,10 @@
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content.</p>
+                            <h3 class="card-title">$ 4.000 Pizarras</h3>
+                            <p class="card-text">Pizarras para divertirse dibujando!</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
@@ -104,16 +103,15 @@
                 <a href="">
                     <div class="card shadow h-100">
                         <picture class="zoom">
-                            <source srcset="img/5.webp" type="image/webp">
-                            <img src="img/5.jpg" class="card-img-top" fetchpriority=high height="100%" width="200"
+                            <source srcset="img/4.webp" type="image/webp">
+                            <img src="img/4.jpg" class="card-img-top" fetchpriority=high height="100%" width="200"
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h3 class="card-title">$ 8.500 Pelotas</h3>
+                            <p class="card-text">Pelotas N° 2 y 5 y de básquet</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
@@ -127,11 +125,10 @@
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h3 class="card-title">$ 10.000 Sets de jardín 2026</h3>
+                            <p class="card-text">Combos, taza, cuchara/tenedor, individual, servilleta, toalla, botella, tupper, mochila, cartuchera y más!!!!</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
@@ -140,16 +137,15 @@
                 <a href="">
                     <div class="card shadow h-100">
                         <picture class="zoom">
-                            <source srcset="img/7.webp" type="image/webp">
-                            <img src="img/7.jpg" class="card-img-top" fetchpriority=high height="100%" width="200"
+                            <source srcset="img/5.webp" type="image/webp">
+                            <img src="img/5.jpg" class="card-img-top" fetchpriority=high height="100%" width="200"
                                 alt="Image1">
                         </picture>
                         <div class="card-body">
-                            <h3 class="card-title">Card title</h3>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+                            <h3 class="card-title">$ 21.000 Monopatines - últimos tres</h3>
+                            <p class="card-text">Estamos a una cuadra de las Mil en el barrio Barrio Independencia Corrientes</p>
                         </div>
+                        <div class="card-footer text-body-secondary">10 de enero</div>
                     </div>
                 </a>
             </div>
--- panel/index.php
+++ panel/index.php
@@ -20,7 +20,7 @@
     <!-- Menú -->
     <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Frani</a>
+            <a class="navbar-brand" href="<?= BASE_URL ?>">Frani</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -28,7 +28,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?= BASE_URL ?>">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>panel">Panel</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
                 </ul>
